348f55096d3894adfeed1c65cf785be8c035b3f0
MonsterGuden <monsterguden@gmail.com>
2012-07-10 20:34:34 +0200

    camera never leave the tilemap

--- src/run.py
+++ src/run.py
@@ -8,7 +8,7 @@ pygame.init()
 pygame.display.set_caption("LPC 2012 Python Game")
 clock = pygame.time.Clock()
 
-level = world.level1()
+level = world.level2()
 screen = pygame.display.set_mode((level.screen_width, level.screen_height))
 rect = screen.get_rect()
 
@@ -20,6 +20,23 @@ level.sprite_layers[1].add_sprite(hero)
 renderer = tiledtmxloader.helperspygame.RendererPygame()
 # set initial cam position and size
 renderer.set_camera_position_and_size(0, 0, level.screen_width, level.screen_height, "topleft")
+
+def update_camera():
+    HeroRect = hero.rect
+    renderer.set_camera_position(HeroRect.centerx, HeroRect.centery)
+    if renderer._cam_rect.top < 0:
+        renderer._cam_rect.top = 0
+        renderer.set_camera_rect(renderer._cam_rect)
+    if renderer._cam_rect.left < 0:
+        renderer._cam_rect.left = 0
+        renderer.set_camera_rect(renderer._cam_rect)
+    if level.world_map.pixel_width < renderer._cam_rect.right:
+        renderer._cam_rect.right = level.world_map.pixel_width
+        renderer.set_camera_rect(renderer._cam_rect)
+    if level.world_map.pixel_height < renderer._cam_rect.bottom:
+        renderer._cam_rect.bottom = level.world_map.pixel_height
+        renderer.set_camera_rect(renderer._cam_rect)
+
 
 while 1:
     deltat = clock.tick(30)
@@ -35,7 +52,7 @@ while 1:
     screen.fill((0, 100, 100))
     # update hero and camera
     hero.update(deltat)
-    renderer.set_camera_position(hero.rect.centerx, hero.rect.centery)
+    update_camera()
 
     # draw the stuff
     for sprite_layer in level.sprite_layers:
--- src/world.py
+++ src/world.py
@@ -2,8 +2,6 @@ import pygame, tiledtmxloader
 
 class World():
     def __init__(self, map):
-        self.screen_width = 1024
-        self.screen_height = 768
         # load the world
         self.world_map = tiledtmxloader.tmxreader.TileMapParser().parse_decode(map)
         # prepare map rendering
@@ -12,10 +10,18 @@ class World():
         self.resources = tiledtmxloader.helperspygame.ResourceLoaderPygame()
         # load images into pygame
         self.resources.load(self.world_map)
+        self.screen_width = min(1024, self.world_map.pixel_width)
+        self.screen_height = min(768, self.world_map.pixel_height)
 
 
 class level1(World):
     def __init__(self):
         World.__init__(self, "priv/maps/level1.tmx")
+        # retrieve layers
+        self.sprite_layers = tiledtmxloader.helperspygame.get_layers_from_map(self.resources)
+
+class level2(World):
+    def __init__(self):
+        World.__init__(self, "priv/maps/level2.tmx")
         # retrieve layers
         self.sprite_layers = tiledtmxloader.helperspygame.get_layers_from_map(self.resources)
