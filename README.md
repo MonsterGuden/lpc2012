2802d196471a846c60ad474a8dff0a4cdfa48bf9
MonsterGuden <monsterguden@gmail.com>
2012-07-31 08:50:21 +0200

    everything should be licensed ok now

--- README.md
+++ README.md
@@ -4,8 +4,18 @@ lpc2012
 Liberated Pixel Cup 2012 Python game
 
 - Requirements:
-aptitude install python-pygame
-aptitude install python-pyglet
+python-pygame, python-pyglet, map loader for 'tiled'
+
+-Installation instructions
+sudo aptitude install python-pygame
+sudo aptitude install python-pyglet
+wget https://pytmxloader.googlecode.com/files/tiledtmxloader-3.0.3.114.zip
+unzip -x tiledtmxloader-3.0.3.114.zip
+cd tiledtmxloader-3.0.3.114
+sudo python setup.py install
+cd ..
+rm -rf tiledtmxloader-3.0.3.114
+rm -rf tiledtmxloader-3.0.3.114.zip
 
 - To run the game:
 In the folder where you found this file run this command:
@@ -14,13 +24,10 @@ python src/run.py
 - Controls:
 f to toggle between fullscreen and window mode
 arrows to move the "character"
+escape to exit
 
 - Creating a map:
 background in tile layer 0
 hero in tile layer 1
 collision objects in tile layer 2
 mission goal in tile layer 3
-
-- Credits so far:
-Roux
-Lucas
--- src/util.py
+++ src/util.py
@@ -1,8 +1,26 @@
+# LPC 2012 Coding competition entry
+# Copyright (C) 2012  Lucas Robsahm
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Defines the different states
 STATE_InGame = 1
 STATE_NewGame = 2
 STATE_GameOver = 3
 STATE_GameComplete = 4
 
+# Get the neighbour tiles to the position rec_center in tile_layer
 def neighbour_tiles(rect_center, tile_layer):
     (pos_x, pos_y) = rect_center
     tile_rects = []
